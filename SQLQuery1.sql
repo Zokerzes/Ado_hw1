b3f7eb2d58c88e95fbdce9a29abee5163b904774
Roman <yozhik13@gmail.com>
2023-02-03 01:05:31 +0300

    Добавьте файлы проекта.

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿--Название;
+--Тип (овощ или фрукт);
+--Цвет;
+--Калорийность.
+
+create table Vegetables_Fruits(
+	id int identity(1,1) not null CONSTRAINT PK_Vegetables_Fruits primary key,
+	NameVF nvarchar(30) not null,
+	TypeVF nvarchar(30) not null,
+	ClolorVF nvarchar(30) not null,
+	Caloric_Content decimal not null
+	)
